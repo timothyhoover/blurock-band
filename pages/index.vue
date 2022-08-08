eb92a6bbf2ebd5fd112babd56c618f3b067e046a
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2022-08-08 00:02:48 -0500

    cms setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on('init', user => {
+    if (!user) {
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/'
+      })
+    }
+  })
+}
+</script>
 
 <template lang="pug">
 hero-section
