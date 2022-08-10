a532c32aed5f17d759569e787b0870411a91dd22
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2022-08-09 23:56:53 -0500

    update responsiveness

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -10,7 +10,9 @@ const { data } = await useAsyncData('home', () => {
 section(
   class='flex flex-col justify-center lg:flex-row lg:justify-between w-full items-center lg:space-x-10 px-5 py-12'
 )
-  div(class='flex flex-col items-center space-y-3 w-full mb-10 lg:mb-0 lg:items-start')
+  div(
+    class='flex flex-col items-center space-y-3 w-full md:w-3/4 lg:w-1/2 mb-10 lg:mb-0 lg:items-start'
+  )
     div(class='flex flex-col')
       h1(class='text-2xl lg:text-3xl font-semibold text-white') {{ data?.title }}
       div(class='self-end bg-br-tertiary h-2 w-3/5')
@@ -25,7 +27,7 @@ section(
       div(class='flex items-center space-x-2')
         p {{ data?.button }}
         icon-calendar(class='w-5')
-  div(class='shadow-md p-2 bg-br-tertiary rounded-md w-full max-w-2xl')
+  div(class='shadow-md p-2 bg-br-tertiary rounded-md w-full md:w-3/4 lg:w-1/2 max-w-2xl')
     iframe(
       class='w-full h-[500px]'
       :src='data?.video'
